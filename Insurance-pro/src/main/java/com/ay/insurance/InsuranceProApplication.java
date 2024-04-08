f040914720f1154b1901d6d11497ec0d97963c84
anuhyab05 <axbejjanki@shockers.wichita.edu>
2024-04-07 19:03:43 -0500

    Added structure of the project

---
+++ Insurance-pro/src/main/java/com/ay/insurance/InsuranceProApplication.java
@@ -0,0 +1,13 @@
+package com.ay.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceProApplication.class, args);
+	}
+
+}
