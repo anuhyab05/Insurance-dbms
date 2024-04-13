de750c09af5e3796b7c09456d7be5f17110d4ef4
anuhya <axbejjanki@shockers.wichita.edu>
2024-04-12 23:40:17 -0500

    Added global exception handler to the project so that it will handle the exceptions and errors will be handled

---
+++ Insurance-pro/src/main/java/com/ay/insurance/controller/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.ay.insurance.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(Exception e, Model model) {
+        model.addAttribute("errorMessage", e.getMessage());
+        return "error"; // The name of the error.html view
+    }
+}
